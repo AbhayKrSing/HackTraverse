ff865202f3ac00e088dfd07a3ed317f3df10b7e9
Abhay <kabhay849@gmail.com>
2023-07-23 23:01:38 +0530

    one more commit

--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -9,8 +9,6 @@ const Home = () => {
             <h1 style={{ textAlign: 'center' }}>Add your journey videos & images here</h1>
             <Card></Card>
             <Distingusiher></Distingusiher>
-            <Card></Card>
-            <Distingusiher></Distingusiher>
 
         </div>
     )
