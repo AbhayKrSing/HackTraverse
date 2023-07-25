a75d51ec5799439e4809ae2ee05c4f307443a756
Abhay <kabhay849@gmail.com>
2023-07-25 13:27:38 +0530

    going to refactor

--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -1,14 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../Navbar/Navbar'
-import Card from '../../Card/Card'
-import Distingusiher from '../Distinguisher'
 import { useNavigate } from 'react-router-dom'
 import { auth } from '../Firebase'
 import { onAuthStateChanged } from 'firebase/auth'
 
 const Home = ({ setLoginUser, LoginUser }) => {
     const navigate = useNavigate()
-    const [filearray, setfilearray] = useState([1])
     useEffect(() => {
         const unsuscribe = onAuthStateChanged(auth, (user) => {
             if (user) {
@@ -27,15 +24,6 @@ const Home = ({ setLoginUser, LoginUser }) => {
         <div>
             <Navbar />
             <h1 style={{ textAlign: 'center' }}>Add your journey videos & images here</h1>
-
-            {filearray.map((element, i) => {
-                return (<div key={i}>
-                    <div style={{ display: `${i > 0 ? 'block' : 'none'}` }}>
-                        <Distingusiher />
-                    </div>
-                    <Card LoginUser={LoginUser} setLoginUser={setLoginUser} filearray={filearray} setfilearray={setfilearray} element={element} />
-                </div>)
-            })}
 
         </div>
     )
