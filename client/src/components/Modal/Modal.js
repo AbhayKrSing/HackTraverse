d57369280bdf801d1cfd5487ce0742b428bf98b2
Abhay <kabhay849@gmail.com>
2023-09-01 10:49:17 +0530

    done with data arrangement

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -8,11 +8,11 @@ const Modal = ({ children, currentImageUrl, imageUrls, setimageUrls, scroll, cur
     const inputref = useRef()
     const [condition, setcondition] = useState(false)
     const [textLength, settextLength] = useState(0)
-
+    const date = new Date()
     const SaveChanges = async () => {
         try {
             if (inputref.current.value && LoginUser) {
-                const storageRef = ref(storage, LoginUser.uid + '/' + currentblob.name); //To upload file(blob) on filebase
+                const storageRef = ref(storage, LoginUser.uid + '/' + currentblob.name + '(' + date.getTime() + ')'); //To upload file(blob) on filebase
                 await uploadBytes(storageRef, currentblob)
                 console.log('File uploaded successfully')
 
@@ -20,7 +20,8 @@ const Modal = ({ children, currentImageUrl, imageUrls, setimageUrls, scroll, cur
                 let inputStr = inputref.current.value        //uploading user's input data(text)
                 inputStr = inputStr.replace(/\s+/g, ' ');
                 const docRef = await addDoc(collection(db, LoginUser.uid), {
-                    data: inputStr
+                    data: inputStr,
+                    timeCreated: date.getTime()
                 });
                 console.log("Document written with ID: ", docRef.id);
                 setcurrentText([...currentText, inputStr])
@@ -49,40 +50,68 @@ const Modal = ({ children, currentImageUrl, imageUrls, setimageUrls, scroll, cur
     }
     useEffect(() => {
         //1.Now for getting all user's blob and file and render it on web page
-        if (LoginUser) {
-            const folderRef = ref(storage, LoginUser.uid);
+        const getfile = async () => {
+            if (LoginUser) {
+                const folderRef = ref(storage, LoginUser.uid);
 
-            // List all files in the folder
-            listAll(folderRef)
-                .then((res) => {
-                    // Loop through the files
-                    res.items.forEach((itemRef) => {
-                        // Get the download URL of the file
-                        getDownloadURL(itemRef).then((url) => {
-                            // Print the file URL
-                            console.log(url);
-                        });
-                    });
-                })
+                // List all files in the folder
+                listAll(folderRef)
+                    .then(async (res) => {
+                        let ItemArray = res.items;
+                        console.log(ItemArray)
+                        let urlArray = []
+                        //Sorting of ItemArray according to time created.
+                        ItemArray.sort((element1, element2) => {
+                            let input_string = element1.name
+                            const num1 = Number(input_string.substring(input_string.lastIndexOf("(") + 1, input_string.lastIndexOf(")")))
+                            input_string = element2.name
+                            const num2 = Number(input_string.substring(input_string.lastIndexOf("(") + 1, input_string.lastIndexOf(")")))
+                            return num1 - num2
+                        })
+                        for (let element of ItemArray) {
+                            const url = await getDownloadURL(element)  //yaha galti ho rhi thi await ka use nhi kiya tha.
+                            urlArray.push(url)
+                        }
+                        setimageUrls([...urlArray])
+                    }).catch((error) => {
+                        console.log(error.message)
+                    })
+            }
+
         }
-
 
 
         //2.for getting all user's text data
         const getdata = async () => {
             if (LoginUser) {
                 const colRef = collection(db, LoginUser.uid);
-                // Get all documents from the collection
                 getDocs(colRef).then((querySnapshot) => {
-                    querySnapshot.forEach((doc) => {
-                        // Print the document ID and data
-                        // console.log(doc.id, " => ", doc.data());
-                        console.log(doc.data())
-                    });
+                    let textArray = []
+                    const Snapshot = querySnapshot.docs;
+                    for (let doc of Snapshot) {
+                        const datatext = doc.data().data
+                        const timeCreated = doc.data().timeCreated
+                        textArray.push({ datatext, timeCreated })
+                    }
+                    textArray.sort((a, b) => {
+                        return a.timeCreated - b.timeCreated
+                    })
+                    textArray = textArray.map((element) => {
+                        return element.datatext
+                    })
+                    setcurrentText([...textArray])
+
+                    return 1;
+                }).catch((error) => {
+                    console.log(error.message)
                 });
             }
         }
-        getdata()
+        const fn = async () => {
+            await Promise.all[getfile(), getdata()]  //fir ispe kaam karna hai
+            console.log('done')
+        }
+        fn()
         // eslint-disable-next-line
     }, [LoginUser])
 
